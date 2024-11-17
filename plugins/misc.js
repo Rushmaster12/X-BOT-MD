7ca77544b8b18fafe9eed5c32ed7affef1c7569b
Rushmaster12 <160727728+Rushmaster12@users.noreply.github.com>
2024-11-17 23:49:43 +0530

    Update misc.js

--- plugins/misc.js
+++ plugins/misc.js
@@ -44,10 +44,10 @@ Sparky(
       
       const start = new Date().getTime();
   
-let pong = await client.sendMessage(m.jid , { text : "_Checking Ping..._" } , { quoted : m })
+let pong = await client.sendMessage(m.jid , { text : "_Checking X-BOT Pinggging..._" } , { quoted : m })
 const end = new Date().getTime();
 
-await client.sendMessage(m.jid, { text : `_Latency : ${end - start} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end - start} ms_` , edit : pong.key })
   /*
     const start = new Date().getTime();
   
@@ -55,42 +55,42 @@ let pong = await client.sendMessage(m.jid , { text : "_Checking Ping..._" } , { 
 const end = new Date().getTime();
       */
       const start1 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end - start} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end - start} ms_` , edit : pong.key })
   
       const end1 = new Date().getTime();
      const start2 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end1 - start1} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end1 - start1} ms_` , edit : pong.key })
       const end2 = new Date().getTime();
       const start3 = new Date().getTime();
 
-await client.sendMessage(m.jid, { text : `_Latency : ${end2 - start2} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end2 - start2} ms_` , edit : pong.key })
       const end3 = new Date().getTime();
       const start4 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end3 - start3} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end3 - start3} ms_` , edit : pong.key })
       const end4 = new Date().getTime();
       const start5 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end4 - start4} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end4 - start4} ms_` , edit : pong.key })
       const end5 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end5 - start5} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end5 - start5} ms_` , edit : pong.key })
 /*
       const start6 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end6 - start6} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end6 - start6} ms_` , edit : pong.key })
       const end6 = new Date().getTime();
 
       const start7 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end7 - start7} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end7 - start7} ms_` , edit : pong.key })
       const end7 = new Date().getTime();
 
   const start8 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end8 - start8} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end8 - start8} ms_` , edit : pong.key })
       const end8 = new Date().getTime();
 
       const start9 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end9 - start9} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end9 - start9} ms_` , edit : pong.key })
       const end9 = new Date().getTime();
 
       const start10 = new Date().getTime();
-await client.sendMessage(m.jid, { text : `_Latency : ${end10 - start10} ms_` , edit : pong.key })
+await client.sendMessage(m.jid, { text : `_ 𝚾-𝚩𝚯𝚻 𝐒𝚸𝚵𝚵𝐃📍 : ${end10 - start10} ms_` , edit : pong.key })
       const end10 = new Date().getTime();
   */
   });
